4ccb2dce693f04e0bbe314531c419e5144d1b844
Abdullah <abdullah056644@hotmail.com>
2022-12-16 18:25:26 +0300

    Create a function that returns ul element

--- src/index.js
+++ src/index.js
@@ -5,3 +5,23 @@ function createContentDiv() {
     content.id = 'content';
     return content;
 }
+
+function createHeader() {
+    const header = document.createElement('ul');
+    header.id = 'header';
+
+    const homeTab = document.createElement('li');
+    homeTab.classList.add('home-tab');
+
+    const menuTab = document.createElement('li');
+    menuTab.classList.add('menu-tab');
+
+    const contactTab = document.createElement('li');
+    contactTab.classList.add('contact-tab');
+
+    header.appendChild(homeTab);
+    header.appendChild(menuTab);
+    header.appendChild(contactTab);
+
+    return header;
+}
