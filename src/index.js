f82a4ae81d00bee8c6fdb254ccbcc39d54b16493
Abdullah <abdullah056644@hotmail.com>
2022-12-16 18:43:10 +0300

    Adjust createHeader function

--- src/index.js
+++ src/index.js
@@ -12,12 +12,15 @@ function createHeader() {
 
     const homeTab = document.createElement('li');
     homeTab.classList.add('home-tab');
+    homeTab.textContent = 'Home';
 
     const menuTab = document.createElement('li');
     menuTab.classList.add('menu-tab');
+    menuTab.textContent = Menu;
 
     const contactTab = document.createElement('li');
     contactTab.classList.add('contact-tab');
+    contactTab.textContent = 'Contact';
 
     header.appendChild(homeTab);
     header.appendChild(menuTab);
