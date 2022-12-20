4560b36f3dbf5b3e9dbdf23bcf511326ad7f57f2
Abdullah <abdullah056644@hotmail.com>
2022-12-20 17:18:44 +0300

    Make tab switching logic

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import contact from './contact';
 const homeContent = home();
 const menuContent = menu();
 const contactContent = contact();
+let currentTab = homeContent;
 
 (function startWebsite() {
     const body = document.querySelector('body');
@@ -14,6 +15,7 @@ const contactContent = contact();
     content.appendChild(header);
     content.appendChild(homeContent);
     body.appendChild(content);
+    switchTabs();
 })();
 
 function createContentDiv() {
@@ -29,6 +31,7 @@ function createHeader() {
     const homeTab = document.createElement('li');
     homeTab.classList.add('home-tab');
     homeTab.textContent = 'Home';
+    homeTab.style.color = '#10b981';
 
     const menuTab = document.createElement('li');
     menuTab.classList.add('menu-tab');
@@ -43,4 +46,53 @@ function createHeader() {
     header.appendChild(contactTab);
 
     return header;
+}
+
+function switchTabs() {
+    const [homeTab, menuTab, contactTab] = document.querySelectorAll('li');
+    homeTab.addEventListener('click', function () {
+        removeTabContent(currentTab);
+        resetColors();
+
+        currentTab = homeContent;
+        homeTab.style.color = '#10b981';
+
+        appendToContent(currentTab);
+    })
+
+    menuTab.addEventListener('click', function () {
+        removeTabContent(currentTab);
+        resetColors();
+
+        currentTab = menuContent;
+        menuTab.style.color = '#10b981';
+
+        appendToContent(currentTab);
+    })
+
+    contactTab.addEventListener('click', function () {
+        removeTabContent(currentTab);
+        resetColors();
+
+        currentTab = contactContent;
+        contactTab.style.color = '#10b981';
+
+        appendToContent(currentTab);
+    })
+
+}
+
+function removeTabContent(tab) {
+    const content = document.getElementById('content');
+    content.removeChild(tab);
+}
+
+function appendToContent(tab) {
+    const content = document.getElementById('content');
+    content.appendChild(tab);
+}
+
+function resetColors() {
+    const tabs = document.querySelectorAll('li');
+    tabs.forEach((elem) => elem.style.color = 'white');
 }
